a768354e569a861521802bb6e5fcf7b1b9038ab9
Matthias Bathke <matthias-bathke@straightvisions.com>
2023-02-08 09:20:07 +0100

    remove border / remove default colors

--- sv_block_media_text.php
+++ sv_block_media_text.php
@@ -40,11 +40,6 @@
 				->set_is_responsive(true)
 				->load_type( 'margin' );
 
-			$this->get_setting( 'border' )
-				->set_title( __( 'Border', 'sv100' ) )
-				->set_is_responsive(true)
-				->load_type( 'border' );
-
 			return $this;
 		}
 	}
